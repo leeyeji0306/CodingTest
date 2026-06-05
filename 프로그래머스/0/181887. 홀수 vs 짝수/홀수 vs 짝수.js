c41241b836ef54fb212e6492951ecec16c19d3b6
이예지 <s2548@e-mirim.hs.kr>
2026-06-05 11:45:32 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.15 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    var answer = 0;
+    let odd = 0;
+    let even = 0;
+    for(let i = 0; i<num_list.length; i++){
+        if((i+1)%2===1){
+            odd+=num_list[i]
+        } else{
+            even+=num_list[i]
+        }
+    }
+    
+    return odd>even ? odd: even;
+}
