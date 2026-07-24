688407789bbefe2d880d6eac376cc7d48bfb6ef0
LEE YE JI <s2548@e-mirim.hs.kr>
2026-07-24 11:33:57 +0900

    [level 1] Title: 예산, Time: 6.08 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,26 +1,11 @@
+import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-        int sum=0;
         int answer = 0;
-        for(int i=0; i<d.length-1; i++){
-            for(int j=0; j<d.length-1; j++){
-                if(d[j]>d[j+1]){
-                    int temp=d[j];
-                    d[j]=d[j+1];
-                    d[j+1]=temp;
-                }
-            }
-        }
+        Arrays.sort(d);
         for(int i=0; i<d.length; i++){
-            sum+=d[i];
-            if(budget<sum){
-                break;
-            }
-            else if(budget==sum){
-                answer++;
-                break;
-            }
-            else{
+            if(budget-d[i] >= 0){
+                budget-=d[i];
                 answer++;
             }
         }
