3c90718de1d93483d3e585001a568948aa8ff2a3
LEE YE JI <s2548@e-mirim.hs.kr>
2026-07-24 10:54:53 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer=0;
         for(int i=0; i<absolutes.length; i++){
             if(signs[i]) answer+=absolutes[i];
-            else answer+=-absolutes[i];
+            else answer-=absolutes[i];
         }
         return answer;
     }
