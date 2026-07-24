220852c646ad2cf75dbce4a025f37bbcda46d8e1
LEE YE JI <s2548@e-mirim.hs.kr>
2026-07-24 10:52:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,22 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        long number=num;
-        while(true){
-            if(answer==500){
-                return -1;
-            }
-            if(number==1){
-                return answer;
-            }
-            if(number%2==0){
-                number/=2;
+        long temp = num;
+        //1. 입력된 수가 짝수라면 2로 나누고, 홀수라면 3을 곱하고 1을 더함.
+        //2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복
+        while(temp != 1){
+            if(temp%2 == 0){
+                temp/=2;
             }
             else{
-                number*=3;
-                number++;
+                temp = temp * 3 + 1;
             }
             answer++;
+            if(answer==500){
+                answer = -1;
+                break;
+            }
         }
+        return answer;
     }
 }
