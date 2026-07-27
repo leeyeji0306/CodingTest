0700ed7263f4ee0ba11d0085933c891796be0f15
LEE YE JI <s2548@e-mirim.hs.kr>
2026-07-27 17:15:40 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 54.90 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        int[] before = new int[arr.length];
+        while(true){
+            before = Arrays.copyOf(arr, arr.length);
+            for(int i=0; i<arr.length; i++){
+                if(arr[i] >= 50 && arr[i] % 2 == 0){
+                    arr[i]/=2;
+                }
+                else if(arr[i] < 50 && arr[i] % 2 ==1){
+                    arr[i] = arr[i]*2+1;
+                }
+            }
+            if(Arrays.equals(before, arr)){
+                return answer;
+            }
+            else{
+                answer++;
+            }
+        }
+    }
+}
