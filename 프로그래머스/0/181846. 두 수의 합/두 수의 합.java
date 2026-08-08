9d708db39c6f1980926c9d25e9998f3e5faa9ec5
LEE YE JI <s2548@e-mirim.hs.kr>
2026-08-08 10:44:58 +0900

    [level 0] Title: 두 수의 합, Time: 343.88 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,11 @@
+import java.math.*;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger bigA = new BigInteger(a);
+        BigInteger bigB = new BigInteger(b);
+        BigInteger sum = bigA.add(bigB);
+        String answer = sum.toString();
+        return answer;
+    }
+}
